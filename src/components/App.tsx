b5ae8629ed7ceb2384aaaf00389566d434487cd8
Stuart Williams <stuart@tickx.co.uk>
2021-12-01 07:28:09 +0000

    chore: add code splitting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,65 @@
+import loadable from "@loadable/component";
 import AppBar from "components/AppBar";
 import IndexPage from "pages";
-import FindTheNotePage from "pages/find-the-note";
-import FretboardReference from "pages/fretboard-reference";
-import NameTheNotePage from "pages/name-the-note";
-import SettingsPage from "pages/settings";
-import StatisticsPage from "pages/statistics";
 import React, { FC } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+const NameTheNote = loadable(
+  () => import(/* webpackChunkName: "name-the-note"*/ "pages/name-the-note"),
+  {
+    fallback: <p>Loading...</p>,
+  }
+);
+
+const FindTheNote = loadable(
+  () => import(/* webpackChunkName: "find-the-note"*/ "pages/find-the-note"),
+  {
+    fallback: <p>Loading...</p>,
+  }
+);
+
+const FretboardReference = loadable(
+  () =>
+    import(
+      /* webpackChunkName: "fretboard-reference"*/ "pages/fretboard-reference"
+    ),
+  {
+    fallback: <p>Loading...</p>,
+  }
+);
+
+const Settings = loadable(
+  () => import(/* webpackChunkName: "settings"*/ "pages/settings"),
+  {
+    fallback: <p>Loading...</p>,
+  }
+);
+
+const Statistics = loadable(
+  () => import(/* webpackChunkName: "statistics"*/ "pages/statistics"),
+  {
+    fallback: <p>Loading...</p>,
+  }
+);
 
 const App: FC = () => (
   <Router>
     <AppBar />
     <Switch>
       <Route path="/name-the-note">
-        <NameTheNotePage />
+        <NameTheNote />
       </Route>
       <Route path="/find-the-note">
-        <FindTheNotePage />
+        <FindTheNote />
       </Route>
       <Route path="/fretboard-reference">
         <FretboardReference />
       </Route>
       <Route path="/settings">
-        <SettingsPage />
+        <Settings />
       </Route>
       <Route path="/statistics">
-        <StatisticsPage />
+        <Statistics />
       </Route>
       <Route path="/">
         <IndexPage />
