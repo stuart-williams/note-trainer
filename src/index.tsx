f565be80764097905102aa32f2fc6dc0d4287177
Stuart Williams <stuart@tickx.co.uk>
2021-12-08 07:52:31 +0000

    feat: inject analytics script

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,33 @@
 import { ChakraProvider, LightMode } from "@chakra-ui/react";
 import App from "components/App";
-import React from "react";
+import React, { FC } from "react";
 import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
 import { RecoilRoot } from "recoil";
 import theme from "theme";
 
+const Analytics: FC = () => (
+  <Helmet>
+    {process.env.NODE_ENV === "production" && (
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-JDKSFDYP9M"
+      />
+    )}
+    <script>{`
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-JDKSFDYP9M");
+    `}</script>
+  </Helmet>
+);
+
 ReactDOM.render(
   <React.StrictMode>
+    <Analytics />
     <ChakraProvider theme={theme}>
       <LightMode>
         <RecoilRoot>
