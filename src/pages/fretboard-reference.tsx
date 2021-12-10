82b961549bbc46be5d5e1ac8d6dbdf94e1869439
Stuart Williams <stuart@tickx.co.uk>
2021-12-10 06:26:25 +0000

    feat: add note filter to fretboard reference

--- src/pages/fretboard-reference.tsx
+++ src/pages/fretboard-reference.tsx
@@ -1,5 +1,6 @@
 import Fretboard from "components/Fretboard";
-import React, { FC, useMemo } from "react";
+import Keyboard from "components/Keyboard";
+import React, { FC, useMemo, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { fretboardNotesState, halfNotesState } from "state";
 import { INote } from "types";
@@ -8,17 +9,32 @@ import { toDisplayNoteName } from "utils";
 const FretboardReference: FC = () => {
   const halfNotes = useRecoilValue(halfNotesState);
   const notes = useRecoilValue(fretboardNotesState);
+  const [noteFilter, setNoteFilter] = useState<string>("");
 
   const activeNotes = useMemo<INote[]>(
     () =>
-      notes.map((note) => ({
-        ...note,
-        displayName: toDisplayNoteName(note.name, halfNotes),
-      })),
-    [halfNotes]
+      notes.reduce<INote[]>(
+        (accum, note) =>
+          !noteFilter || note.name === noteFilter
+            ? [
+                ...accum,
+                {
+                  ...note,
+                  displayName: toDisplayNoteName(note.name, halfNotes),
+                },
+              ]
+            : accum,
+        []
+      ),
+    [noteFilter, halfNotes]
   );
 
-  return <Fretboard activeNotes={activeNotes} />;
+  return (
+    <>
+      <Fretboard activeNotes={activeNotes} />
+      <Keyboard alignSelf="center" onClick={setNoteFilter} />
+    </>
+  );
 };
 
 export default FretboardReference;
