47dc59f823390105d85e2806ea55e709504a0780
Stuart Williams <stuart@tickx.co.uk>
2021-12-27 08:54:09 +0000

    chore: tidy game countdown

--- src/components/GameCountdown.tsx
+++ src/components/GameCountdown.tsx
@@ -2,9 +2,10 @@ import {
   AlertDialog,
   AlertDialogBody,
   AlertDialogContent,
-  AlertDialogHeader,
   AlertDialogOverlay,
   Heading,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
 import { identity } from "lodash";
 import React, { FC, useEffect, useRef } from "react";
@@ -21,7 +22,7 @@ const GameCountdown: FC<Props> = ({ isOpen, onClose }) => {
 
   useEffect(() => {
     if (isOpen) {
-      countdown.start(4000);
+      countdown.start(3000);
     }
   }, [isOpen]);
 
@@ -33,12 +34,10 @@ const GameCountdown: FC<Props> = ({ isOpen, onClose }) => {
       leastDestructiveRef={cancelRef}
     >
       <AlertDialogOverlay>
-        <AlertDialogContent mx={4} textAlign="center">
-          <AlertDialogHeader>Get Ready!</AlertDialogHeader>
-          <AlertDialogBody pb={6}>
-            <Heading size="2xl">
-              {countdown.remaining.seconds - 1 || "GO"}
-            </Heading>
+        <AlertDialogContent w="auto">
+          <AlertDialogBody as={VStack} py={4} px={8}>
+            <Heading size="4xl">{countdown.remaining.seconds}</Heading>
+            <Text fontSize="xl">GET READY</Text>
           </AlertDialogBody>
         </AlertDialogContent>
       </AlertDialogOverlay>
